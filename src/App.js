66c53e2413a49374b3fbf106824b88860b5b584c
brainconnect93 <brainconnect0@gmail.com>
2023-09-27 17:49:48 +0100

    Working with props in file components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
   const expenses = [
@@ -26,31 +26,28 @@ function App() {
       amount: 450,
       date: new Date(2021, 5, 12),
     },
-  ];
+  ]
+
   return (
     <div>
       <h2>Let's Get Started</h2>
       <div>
-        <ExpenseItem
-          title={expenses[0].title}
-          amount={expenses[0].amount}
-          date={expenses[0].date}
+        <Expenses items={expenses} />
+
+        {/* SECOND APPROACH */}
+
+        {/* <ExpenseItem
+          expenses={expenses[0]}
         />
         <ExpenseItem
-          title={expenses[1].title}
-          amount={expenses[1].amount}
-          date={expenses[1].date}
+          expenses={expenses[1]}
         />
         <ExpenseItem
-          title={expenses[2].title}
-          amount={expenses[2].amount}
-          date={expenses[2].date}
+          expenses={expenses[2]}
         />
         <ExpenseItem
-          title={expenses[3].title}
-          amount={expenses[3].amount}
-          date={expenses[3].date}
-        />
+          expenses={expenses[3]}
+        /> */}
       </div>
     </div>
   );
