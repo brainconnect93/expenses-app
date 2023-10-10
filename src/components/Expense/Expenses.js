d8db9a77d4976940e3d7d7215afe97ef4acc66a1
brainconnect93 <brainconnect0@gmail.com>
2023-10-10 10:21:58 +0100

    Render List and conditional content

--- src/components/Expense/Expenses.js
+++ src/components/Expense/Expenses.js
@@ -15,26 +15,12 @@ const Expenses = (props) => {
     <div>
       <Card className="expenses">
         <ExpensesFilter onChangeYear={filterYear} onSelectYear={filterChangeHandler} />
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((product) => <ExpenseItem 
+          key={product.id}
+          title={product.title}
+          amount={product.amount}
+          date={product.date}
+        />)}
       </Card>
     </div>
   );
